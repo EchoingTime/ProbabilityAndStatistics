7913a0b0b4b05a1d383d027b425be8053cd1f74f
dante_anz <dante_anz@localhost>
2023-12-14 23:41:15 -0500

    Updated Commenting

--- Project Two/src/stocks/Main.java
+++ Project Two/src/stocks/Main.java
@@ -1,7 +1,12 @@
 package stocks;
 
 import java.io.FileNotFoundException;
-
+/**
+ * Main Class - Passes through a Stock Historical CSV File and passes through a balance
+ * for StockDataLoader class
+ * @author Dante Anzalone
+ * @version 2023-09 (4.29.0) 
+ */
 public class Main 
 {
 	public static void main (String [] args) throws FileNotFoundException
